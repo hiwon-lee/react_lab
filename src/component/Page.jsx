f694ad5c577eda4c5a985d9f4c38fd43185dfbac
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-10-07 10:28:10 +0900

    docs : useContext 설명 주석석

--- src/component/Page.jsx
+++ src/component/Page.jsx
@@ -5,10 +5,10 @@ import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
 //  현재 isDark라는 애가 너무 많이 사용되고 있어
-// 중간 component다.
+// 중간 component다.(isDark를 사용하지 않음)
 export default function Page() {
   // 이렇게 컨텍스트를 받아올 수 있다. (app.jsx에서 provider로 넘긴 값이..)
-  const data = useContext(ThemeContext);
+  //   const data = useContext(ThemeContext);
 
   return (
     <div className="page">
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,3 +1,3 @@
 import { createContext } from 'react'; // context를 가져오기 위해 필요한 거
 
-export const ThemeContext = createContext(null);
+export const ThemeContext = createContext(null); // null : 우선 아무 값도 넣지 않겠다는 의미, 초기값
