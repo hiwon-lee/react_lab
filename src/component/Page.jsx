217d5fa3c8c41ef6b9e677e37c3388213c269e26
heewon lee <84234411+hiwon-lee@users.noreply.github.com>
2024-10-07 10:16:12 +0900

    docs : props drilling에 대한 설명

--- src/component/Page.jsx
+++ src/component/Page.jsx
@@ -2,9 +2,11 @@ import Content from './Content';
 import Header from './Header';
 import Footer from './Footer';
 
+//  현재 isDark라는 애가 너무 많이 사용되고 있어
+// 중간 component다.
 export default function Page({ isDark, setIsDark }) {
   return (
-    <div>
+    <div className="page">
       <Header isDark={isDark} />
       <Content isDark={isDark} />
       <Footer
